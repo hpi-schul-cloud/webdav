027e64fe36861110b90686ab3f6d78fa18ff6e8b
Matti <contact@schmidtchen.me>
2020-10-29 15:38:44 +0100

    respond to url get requests by nextcloud

--- index.ts
+++ index.ts
@@ -41,6 +41,47 @@ server.setFileSystem('shared', new WebFileSystem('shared'), (succeeded) => {
 
 const app = express()
 
+app.get('/nextcloud/status.php', (req, res) => {
+    // TODO: Answer with real data
+    res.send({
+        installed: true,
+        maintenance: false,
+        needsDbUpgrade: false,
+        version: "10.0.3.3",
+        versionstring: "10.0.3",
+        edition: "Community",
+        productname: "HPI Schul-Cloud"
+    })
+})
+
+app.get('/ocs/v1.php/cloud/capabilities?format=json', (req, res) => {
+    res.send({
+        ocs: {
+            data: {
+                capabilities: {
+                    dav: {
+                        chunking: "1.0"
+                    },
+                    core: {
+                        'webdav-root' : environment.WEBDAV_ROOT,
+                        status: {
+                            edition: "Community",
+                            installed: "true",
+                            needsDbUpgrade: "false",
+                            versionstring: "10.0.3",
+                            productname: "HPI Schul-Cloud",
+                            maintenance: "false",
+                            version : "10.0.3.3"
+                        },
+                        pollinterval: 60
+                    }
+                }
+            }
+        }
+    })
+    // TODO: Send server capabilities as JSON
+})
+
 // root path doesn't seem to work that easily with all webdav clients, if it doesn't work simply put an empty string there
 app.use(webdav.extensions.express(environment.WEBDAV_ROOT, server))
 
